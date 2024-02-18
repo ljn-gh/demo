d4c54a2ccace1a46cfe18c0373c3bd6e4f577364
lijianan-1 <lijianan-1@thunisoft.com>
2024-02-18 09:20:42 +0800

    文件提交

---
+++ src/test/java/com/jianan/demomodule/DemoModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jianan.demomodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
